08a6159952a3c433fcc81f4fd9e5882ce992e879
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-01-10 17:20:25 +0100

    Copy SevenDayPerMillion from next-netlify-starter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
+import {Table} from "./Table.js";
+
+const SevenDayPerMillion = ({json}) => {
+  let dates = Object.keys(json.Totalt_antal_fall);
+  let headers = Object.keys(json);
+
+  return (
+    <Table
+      headers={headers}
+      dates={dates}
+      columns={headers.map((header) => dates.map((date) => json[header][date]))}
+      f={(a, pop) => (a.slice(-7).reduce((a, b) => a + b, 0) / 7 / pop) * 1e6}
+    />
+  );
+}
 
 function App() {
+  const [json, setJson] = useState({Totalt_antal_fall: {}});
+
+  useEffect(() => {
+    fetch("/.netlify/functions/fauna").then(
+      (faunaResp) => {
+        if (!faunaResp.ok) {
+          return faunaResp.text().then((error) => {
+          });
+        }
+
+        return faunaResp.json().then(
+          (json) => {
+            setJson(json);
+          },
+          (error) => {
+            console.log('error', error)
+          }
+        );
+      }
+    );
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <SevenDayPerMillion json={json}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Table.js
@@ -0,0 +1,37 @@
+import React from "react";
+import population from "./population";
+
+export const Table = ({ headers, dates, columns, f }) => {
+  return (
+    <div
+      className="table"
+      style={{
+        "grid-template-columns": `max-content repeat(${headers.length}, 1fr)`,
+        "grid-template-rows": `repeat(${dates.length + 1}, 1fr)`,
+      }}
+    >
+      <span className="date" />
+      {dates.reverse().map((date) => (
+        <span className="date">{date}</span>
+      ))}
+      {columns.map((column, colIndex) => (
+        <>
+          <span className="header">{headers[colIndex]}</span>
+          {column.map((cell, rowIndex) => {
+            let a = column.slice(rowIndex - 13, rowIndex + 1);
+            let x = f(a, population[colIndex]);
+            if (x === undefined) return <span> </span>;
+            return <span className={color(x)}>{Math.round(x)}</span>;
+          }).reverse()}
+        </>
+      ))}
+    </div>
+  );
+
+  function color(x) {
+    for (let i = 960; i >= 60; i /= 2) if (x > i) return "color" + i;
+    if (x > 20) return "color20";
+    if (x > 0) return "color1";
+    return "color0";
+  }
+};
